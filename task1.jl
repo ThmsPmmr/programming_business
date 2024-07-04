873712dc0b48a9b63342792a8e8bc598c246b7c2
ThmsPmmr <thomas.pammer00@gmx.at>
2024-07-04 16:48:00 +0200

    cplex model implemented but does not find an optimal solution yet

--- task1.jl
+++ task1.jl
@@ -1,6 +1,8 @@
-# # Hardcode für die gegebenen Daten
-# using Random
-# using DelimitedFiles
+# Hardcode für die gegebenen Daten
+using Random
+using DelimitedFiles
+using CPLEX
+using JuMP
 
 # #read the instance
 # function read_instance_data(file_path)
@@ -62,9 +64,59 @@ test_file = read_instance(file_path)
 
 #getting the data needed in order to solve this constraint
 
-n, m, Q, W = read_instance_data(file_path)
-V = 1:n
-W[3,3]
+n, m, Q, arcs, costs = read_instance(file_path)
+nodes = 1:n
+#generating the data needed to solve the problem
+predecessors = Vector{Vector{Int64}}(undef,n)
+successors = Vector{Vector{Int64}}(undef, n)
+for i in 1:n
+    predecessors[i] = Vector{Int64}(undef, 0)
+    successors[i] = Vector{Int64}(undef, 0)
+end
 
 
+for arc in arcs
+    pre, suc = arc
+    push!(predecessors[suc], pre)
+    push!(successors[pre], suc)
+end
 
+function solve_ILP(n::Int64, Q::Vector{Int64}, arcs::Vector{Vector{Int64}}, pred::Vector{Vector{Int64}}, succ::Vector{Vector{Int64}}, costs::Matrix{Float64})
+
+    #create model
+    model = Model(CPLEX.Optimizer)
+    set_silent(model)
+    #Decision Variable
+    @variable(model, x[1:n, 1:n], Int)#here we already fullfill constraint 5
+
+    #objective function
+    #we want to minimize the total costs
+    @objective(model, Min, sum(costs[i,j] * x[i,j] for arc in arcs for (i, j) in [(arc[1], arc[2])]))
+    
+    #each node has to be visited as often as i is left
+    @constraint(model, [v=1:n], sum(x[i, v] for i in pred[v]) == sum(x[v, j] for j in succ[v]))
+    
+    # each location which has to be visited is visited at leaste once
+    @constraint(model, [v in Q], sum(x[i, v] for i in pred[v]) >= 1)
+
+    optimize!(model)
+
+    termination_status = JuMP.termination_status(model)
+    primal_status = JuMP.primal_status(model)
+
+    println("Termination status: ", termination_status)
+    println("Primal status: ", primal_status)
+
+    if termination_status != MOI.OPTIMAL
+        println("The solver did not find an optimal solution.")
+        return nothing
+    end
+    #print(objective_value(model))
+  return value.(x)
+end
+solve_ILP(n, Q, arcs, predecessors, successors, costs)
+for arc in arcs
+    i, j = arc
+    println(i)
+    println(j)
+end
