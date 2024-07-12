84ad296d61b269c3318feb82b3eb10cd37b1d1ea
ThmsPmmr <thomas.pammer00@gmx.at>
2024-07-12 09:12:54 +0200

    at least solution is found

--- task1.jl
+++ task1.jl
@@ -91,9 +91,9 @@ function solve_ILP(n::Int64, Q::Vector{Int64}, arcs::Vector{Vector{Int64}}, pred
 
     #objective function
     #we want to minimize the total costs
-    @objective(model, Min, sum(costs[i,j] * x[i,j] for arc in arcs for (i, j) in [(arc[1], arc[2])]))
+    @objective(model, Min, sum(costs[i,j] * x[i,j] for (i, j) in arcs))
     
-    #each node has to be visited as often as i is left
+    #each node i has to be visited as often as i is left
     @constraint(model, [v=1:n], sum(x[i, v] for i in pred[v]) == sum(x[v, j] for j in succ[v]))
     
     # each location which has to be visited is visited at leaste once
@@ -113,6 +113,44 @@ function solve_ILP(n::Int64, Q::Vector{Int64}, arcs::Vector{Vector{Int64}}, pred
 end
 solve_ILP(n, Q, arcs, predecessors, successors, costs)
 
+
+
+###########find all tours
+
+function find_all_tours(x::Matrix{Float64}, arcs::Vector{Vector{Int64}}, n::Int64)
+    used_arcs = [Vector{Int64}() for _ in 1:n]
+    for (i,j) in arcs
+        if value(x[i,j]) > 0.5
+            push!(used_arcs[i], j)
+        end
+    end
+    println(used_arcs)
+
+    function find_connections(c::Int64, subtour::Vector{Int64})
+        push!(subtour, c)
+        visited[c] = true
+        for con in used_arcs[c]
+            if !visited[con]
+                find_connections(con, subtour)
+            end
+        end        
+    end
+    #now lets look for the subtours we currently have
+    all_components = Vector{Vector{Int64}}()
+    visited = falses(n)
+    for u in 1:n
+        if length(used_arcs[u]) >= 1 && !visited[u]
+            #for all locations with connections in the current solution we compute the subtours
+            subtour = Vector{Int64}(undef,0)
+            find_connections(u, subtour)
+            push!(all_components, subtour)
+        end
+    end
+    println(all_components)
+    return all_components
+end
+
+xopt = value.(model[:x])
 # ###################################################################################################
 #           computeTour
 #
@@ -127,52 +165,91 @@ solve_ILP(n, Q, arcs, predecessors, successors, costs)
 # ###################################################################################################
 
 function computeTour(model::Model, startLocation::Int64)
-    optimize!(model)
-    xopt = round.(Int64,value.(model[:x])) # we retrieve the optimal x
+    xopt = round.(Int64,value.(model[:x]))
+    println("x", xopt) # we retrieve the optimal x
     tour = [startLocation] # we initialize the tour with the start location
     
     # iteration 1
     currentLocation = startLocation
     nextLocation = findfirst(!isequal(0), xopt[currentLocation,:])
     push!(tour, nextLocation)
+    println("tour: ", tour)
 
     # fill the rest of the tour
     while nextLocation != startLocation
+        
         currentLocation = nextLocation
-        nextLocation = findfirst(!isequal(0), xopt[currentLocation,:])
+        #println("current location: ", currentLocation)
+        nextLocation = findall(!isequal(0), xopt[currentLocation,:])
         push!(tour, nextLocation)
+        #println("in compute tour", tour)
     end
 
     return tour
 end
 
-function connect_solution(model::Model, Q::Vector{Int64}, n::Int64, succ::Vector{Vector{Int64}})
-    #solve the problem
-    print("Optimiiiiiiiiiiiiert!")
+
+
+function connect_solution(model::Model, Q::Vector{Int64}, n::Int64, succ::Vector{Vector{Int64}}, arcs::Vector{Vector{Int64}})
     all_tours = Vector{Vector{Int64}}()
     subtour_detected = true
     while subtour_detected
+        #all_tours = Vector{Vector{Int64}}()
+        println("vorm optimieren")
+        optimize!(model)
+        println("optimiert")
         #set all nodes to unvisited again 
-        visited = falses(n)
-        subtour_detected = false
-        for i in Q
-            if !visited[i]
-                sub = computeTour(model, i)
-                push!(all_tours, sub)
-                for j in sub[1:end-1]
-                    visited[j] = true
-                end
-                if !all(in(sub), Q)
-                     @constraint(model, [i in sub[1:end-1]], sum(model[:x][i, j] for j in succ[i] if !(j in sub[1:end-1])) >= 1) 
-                     subtour_detected = true
+        #visited = falses(n)
+    
+
+        xopt = value.(model[:x])
+        all_tours = find_all_tours(xopt, arcs, n)
+
+        # for i in Q
+        #     if !visited[i]
+        #         println("before computing tour")
+        #         sub = computeTour(model, i)
+        #         println("subtour: ", sub)
+        #         println("after computing tour")
+        #         push!(all_tours, sub)
+        #         for j in sub[1:end-1]
+        #             visited[j] = true
+        #         end
+        #         # if !all(in(sub), Q)
+        #         #      @constraint(model, [i in sub[1:end-1]], sum(model[:x][i, j] for j in succ[i] if !(j in sub[1:end-1])) >= 1) 
+        #         #      subtour_detected = true
+        #         # end
+        #     else println(i)
+        #     end
+        # end
+        println("before subtour constraints")
+        for subtour in all_tours
+            println(subtour)
+            if length(subtour) > 1
+                if !all(in(subtour), Q) && subtour_detected
+                    println("herinnen")
+                    @constraint(model, sum(model[:x][i, j] for i in subtour for j in succ[i] if !(j in subtour[1:end])) >= 1) 
+                    subtour_detected = true
+                else subtour_detected = false
                 end
             end
         end
         
     end
+    println("heraussen")
+    optimize!(model)
+    obj_value = objective_value(model)
+    println("Total distance traveled: ", obj_value)
+    println("Optimal solution found.")
     
-    return all_tours[end]
+    return(value.(model[:x]))
 end
 model = solve_ILP(n, Q, arcs, predecessors, successors, costs)
-current_solution = connect_solution(model, Q, n, successors)
 
+println(Q)
+current_solution = connect_solution(model, Q, n, successors, arcs)
+#computeTour(model, Q[1])
+
+for (i,j) in arcs
+    println(i , " ", j)
+end
